c564e2115e782b11b0aa95fd1144f4c02c4ed380
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-11-04 16:39:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Start
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 music1 = "music.mp3";
 music2 = "music2.mp3";
 
@@ -13,8 +18,22 @@ function setup() {
 
 	video = createCapture(VIDEO);
 	video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded() {console.log('posenet loaded!');}
 
 function draw() {
     image(video,100,100,600,500);
+}
+
+function gotPoses(results) {
+    if (results > 0) {
+        leftWristX = pose.leftWrist.x;
+        leftWristY = pose.leftWrist.y;
+        rightWristX = pose.rightWrist.x;
+        rightWristY = pose.rightWrist.y;
+    }
 }
