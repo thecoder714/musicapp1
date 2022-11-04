ce6406c4b32d44f750fef0a7553672dd649bf5f6
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-11-04 16:32:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Start
+music1 = "music.mp3";
+music2 = "music2.mp3";
+
+function preload() {
+    loadSound(music1);
+    loadSound(music2);
+}
+
+function setup() {
+    canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function draw() {
+    image(video,100,100,600,500);
+}
