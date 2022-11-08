ad8e8aa1f3ac17883bc233f4a00b429e000b7e57
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-11-08 16:51:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Start
 leftWristX = 0;
 leftWristY = 0;
+scoreLeft = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreRight = 0;
+songStatus = "";
 
 music1 = "music.mp3";
 music2 = "music2.mp3";
@@ -27,6 +30,19 @@ function modelLoaded() {console.log('posenet loaded!');}
 
 function draw() {
     image(video,100,100,600,500);
+    fill("#FF0000");
+	stroke("#FF0000");
+
+    songStatus = music1.isPlaying()
+    
+    if (scoreLeftWrist > 0.2) {
+        music2.stop();
+        circle(rightWristX,rightWristY,20);
+    }
+
+    if (songStatus = false) {
+        music1.play();
+    }
 }
 
 function gotPoses(results) {
@@ -36,4 +52,8 @@ function gotPoses(results) {
         rightWristX = pose.rightWrist.x;
         rightWristY = pose.rightWrist.y;
     }
+
+    scoreRight =  results[0].pose.keypoints[10].score;
+	scoreLeft =  results[0].pose.keypoints[9].score;
+
 }
